64eba978b4e8c06867fdc6d7b8413e9c98904132
markoboras0712 <mboras1@etfos.hr>
2021-04-25 18:25:32 +0300

    stubbing child components

--- tests/unit/MessageDisplay.spec.js
+++ tests/unit/MessageDisplay.spec.js
@@ -12,23 +12,27 @@ beforeEach(() => {
 describe("MessageDisplay", () => {
   it("Calls getMessage once and displays message", async () => {
     const mockMessage = "Hello from the db";
-    getMessage.mockResolvedValueOnce({ text: mockMessage });
+    getMessage.mockResolvedValueOnce({ text: mockMessage }); //mock the API call
     const wrapper = mount(MessageDisplay);
-
+    //wait for promise to resolve
     await flushPromises();
+    //check that call happened once
     expect(getMessage).toHaveBeenCalledTimes(1);
-
+    //check that component displays message
     const message = wrapper.find('[data-testid="message"]').text();
     expect(message).toEqual(mockMessage);
   });
 
   it("Displays an error when getMessage call fails", async () => {
+    //mock the failed API
     const mockError = "Oops! Something went wrong.";
     getMessage.mockRejectedValueOnce(mockError);
     const wrapper = mount(MessageDisplay);
-
+    //wait for promise to resolve
     await flushPromises();
+    //check that call happened once
     expect(getMessage).toHaveBeenCalledTimes(1);
+    //check that component displays error
     const displayedError = wrapper.find('[data-testid="message-error"]').text();
     expect(displayedError).toEqual(mockError);
   });
